b2b93c923c7e391359ec3eacc961308933a22e99
Nedelec Guillaume <guillaume.nedelec@etu.u-bordeaux.fr>
2016-12-15 10:05:45 +0100

    modif

--- assets/index.php
+++ assets/index.php
@@ -1,12 +1,3 @@
-<?php
-include("header.php");
-?>
-/**
- * Created by PhpStorm.
- * User: Thomas
- * Date: 15/12/2016
- * Time: 09:16
- */
- <?php include "menu.php" ?>
-
- <?php include "footer.php" ?>
+<?php include("header.php"); ?>
+<?php include("menu.php"); ?>
+<?php include("footer.php");?>
