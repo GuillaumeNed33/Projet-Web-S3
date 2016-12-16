37e00cfa1b779ff61d866b80b68dd7aec7352fbe
Guillaume Nedelec <guillaume.nedelec@etu.u-bordeaux.fr>
2016-12-16 01:16:19 +0100

    ajout navbar

--- assets/header.php
+++ assets/header.php
@@ -14,7 +14,102 @@
 </head>
 <body>
   <header>
-    <nav class="navbar">
+      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div class="container">
 
-    </nav>
+              <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                      <i class="icon-menu"></i> Menu
+                  </button>
+                  <a class="navbar-brand" href="/">Start Bootstrap</a>
+              </div>
+
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                      <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Our Collection of Templates &amp; Themes">Themes <b class="caret"></b></a>
+                          <ul class="dropdown-menu">
+                              <li>
+                                  <a href="#"><i class="icon-layers fa-fw"></i> All Templates &amp; Themes</a>
+                              </li>
+                              <li>
+                                  <a href="#"><i class="icon-star fa-fw"></i> Most Popular</a>
+                              </li>
+                              <li>
+                                  <a href="#"><i class="icon-handbag fa-fw"></i> Buy Bootstrap Templates</a>
+                              </li>
+                              <li class="divider"></li>
+                              <li class="dropdown-header">
+                                  Template &amp; Theme Categories:
+                              </li>
+                              <li>
+                                  <a href="#">Admin and Dashboard</a>
+                              </li>
+                              <li>
+                                  <a href="#">Full Websites</a>
+                              </li>
+                              <li>
+                                  <a href="#">Landing Pages</a>
+                              </li>
+                              <li>
+                                  <a href="#">One Page Websites</a>
+                              </li>
+                              <li>
+                                  <a href="#">Portfolios</a>
+                              </li>
+                              <li>
+                                  <a href="#">Blogs</a>
+                              </li>
+                              <li>
+                                  <a href="#">Ecommerce</a>
+                              </li>
+                              <li>
+                                  <a href="#">Unstyled Starter Templates</a>
+                              </li>
+                              <li>
+                                  <a href="#">Navigation and Navbars</a>
+                              </li>
+                          </ul>
+                      </li>
+
+                      <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Bootstrap Related Resources">Resources <b class="caret"></b></a>
+                          <ul class="dropdown-menu">
+                              <li>
+                                  <a href="#"><i class="icon-list"></i> Bootstrap Resources List</a>
+                              </li>
+                              <li>
+                                  <a href="#"><i class="icon-support"></i> Help &amp; FAQ</a>
+                              </li>
+                          </ul>
+                      </li>
+                      <li>
+                          <a href="#" title="Start Bootstrap News, Guides, and Tutorials">Articles</a>
+                      </li>
+                      <li>
+                          <a href="#" title="Contact the Start Bootstrap Team">Contact</a>
+                      </li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right">
+                      <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Premium Bootstrap Themes &amp; Templates"><i class="icon-star"></i> Premium <b class="caret"></b></a>
+                          <ul class="dropdown-menu">
+                              <li>
+                                  <a href="#"><i class="icon-pencil fa-fw"></i> Custom Bootstrap Design Services</a>
+                              </li>
+                              <li>
+                                  <a href="https://wrapbootstrap.com/?ref=StartBootstrap"><i class="icon-handbag fa-fw"></i> WrapBootstrap - Premium Bootstrap Themes</a>
+                              </li>
+                          </ul>
+                      </li>
+                      <li>
+                          <a href="#" data-toggle="modal" data-target="#searchModal"><i class="icon-magnifier"></i> <span class="hidden-lg hidden-md hidden-sm">Search Themes</span></a>
+                      </li>
+                  </ul>
+              </div>
+
+          </div>
+
+      </nav>
   </header>
