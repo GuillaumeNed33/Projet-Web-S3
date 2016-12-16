be61dc32e8b829b9a57ace23db928800e90894c8
Thomas Blanc <thomas.blanc.2@etu.u-bordeaux.fr>
2016-12-16 12:19:12 +0100

    Modif navbar

--- assets/header.php
+++ assets/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Bootstrap Example</title>
+    <title>Projet Web</title>
     <!-------------------------------------META----------------------------------------->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,80 +15,29 @@
 <body>
   <header>
       <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-          <div class="container">
+          <div class="container-fluid">
 
               <div class="navbar-header">
                   <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                       <span class="sr-only">Toggle navigation</span>
                       <i class="icon-menu"></i> Menu
                   </button>
-                  <a class="navbar-brand" href="/">Start Bootstrap</a>
+                  <a class="navbar-brand" href="/">Projet Web</a>
               </div>
 
               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                   <ul class="nav navbar-nav">
                       <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Our Collection of Templates &amp; Themes">Themes <b class="caret"></b></a>
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Alphabétique<b class="caret"></b></a>
                           <ul class="dropdown-menu">
-                              <li>
-                                  <a href="#"><i class="icon-layers fa-fw"></i> All Templates &amp; Themes</a>
-                              </li>
-                              <li>
-                                  <a href="#"><i class="icon-star fa-fw"></i> Most Popular</a>
-                              </li>
-                              <li>
-                                  <a href="#"><i class="icon-handbag fa-fw"></i> Buy Bootstrap Templates</a>
-                              </li>
-                              <li class="divider"></li>
-                              <li class="dropdown-header">
-                                  Template &amp; Theme Categories:
-                              </li>
-                              <li>
-                                  <a href="#">Admin and Dashboard</a>
-                              </li>
-                              <li>
-                                  <a href="#">Full Websites</a>
-                              </li>
-                              <li>
-                                  <a href="#">Landing Pages</a>
-                              </li>
-                              <li>
-                                  <a href="#">One Page Websites</a>
-                              </li>
-                              <li>
-                                  <a href="#">Portfolios</a>
-                              </li>
-                              <li>
-                                  <a href="#">Blogs</a>
-                              </li>
-                              <li>
-                                  <a href="#">Ecommerce</a>
-                              </li>
-                              <li>
-                                  <a href="#">Unstyled Starter Templates</a>
-                              </li>
-                              <li>
-                                  <a href="#">Navigation and Navbars</a>
-                              </li>
-                          </ul>
-                      </li>
-
-                      <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Bootstrap Related Resources">Resources <b class="caret"></b></a>
-                          <ul class="dropdown-menu">
-                              <li>
-                                  <a href="#"><i class="icon-list"></i> Bootstrap Resources List</a>
-                              </li>
-                              <li>
-                                  <a href="#"><i class="icon-support"></i> Help &amp; FAQ</a>
-                              </li>
+                              <li><a href"#">Compositeurs</a></li>
+                              <li><a href="#">Interprètes</a></li>
+                              <li><a href="#">Chefs d'Orchestre</a></li>
+                              <li><a href="#">Orchestres</a></li>
                           </ul>
                       </li>
                       <li>
-                          <a href="#" title="Start Bootstrap News, Guides, and Tutorials">Articles</a>
-                      </li>
-                      <li>
-                          <a href="#" title="Contact the Start Bootstrap Team">Contact</a>
+                          <a href="#" title="A propos, rapport du projet">A Propos</a>
                       </li>
                   </ul>
                   <ul class="nav navbar-nav navbar-right">
