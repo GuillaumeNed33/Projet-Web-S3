3ca7c504ea127c2ce9c2b02a62fa75f33ec83a45
Guillaume Nedelec <guillaume.nedelec@etu.u-bordeaux.fr>
2016-12-19 00:35:32 +0100

    clean navbar

--- assets/header.php
+++ assets/header.php
@@ -14,14 +14,14 @@
 </head>
 <body>
   <header>
-      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
           <div class="container-fluid">
 
               <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                       <span class="sr-only">Toggle navigation</span>
                       <i class="icon-menu"></i> Menu
-                  </button>
+                  </button>-->
                   <a class="navbar-brand" href="/">Projet Web</a>
               </div>
 
@@ -30,35 +30,32 @@
                       <li class="dropdown">
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Alphabétique<b class="caret"></b></a>
                           <ul class="dropdown-menu">
-                              <li><a href"#">Compositeurs</a></li>
+                              <li><a href="#">Compositeurs</a></li>
                               <li><a href="#">Interprètes</a></li>
                               <li><a href="#">Chefs d'Orchestre</a></li>
                               <li><a href="#">Orchestres</a></li>
                           </ul>
                       </li>
-                      <li>
-                          <a href="#" title="A propos, rapport du projet">A Propos</a>
-                      </li>
-                  </ul>
-                  <ul class="nav navbar-nav navbar-right">
                       <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" title="Premium Bootstrap Themes &amp; Templates"><i class="icon-star"></i> Premium <b class="caret"></b></a>
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Epoque<b class="caret"></b></a>
                           <ul class="dropdown-menu">
-                              <li>
-                                  <a href="#"><i class="icon-pencil fa-fw"></i> Custom Bootstrap Design Services</a>
-                              </li>
-                              <li>
-                                  <a href="https://wrapbootstrap.com/?ref=StartBootstrap"><i class="icon-handbag fa-fw"></i> WrapBootstrap - Premium Bootstrap Themes</a>
-                              </li>
+                              <li><a href"#">Compositeurs</a></li>
+                              <li><a href="#">Interprètes</a></li>
                           </ul>
                       </li>
                       <li>
-                          <a href="#" data-toggle="modal" data-target="#searchModal"><i class="icon-magnifier"></i> <span class="hidden-lg hidden-md hidden-sm">Search Themes</span></a>
+                          <a href="#">Instruments</a>
                       </li>
+                      <li>
+                          <a href="#">Genre</a>
+                      </li>
+                  </ul>
+
+                  <ul class="nav navbar-nav navbar-right">
+                      <li><a href="#">Connexion</a></li>
                   </ul>
               </div>
 
           </div>
-
       </nav>
   </header>
