c665cd118849f39563102fbc03878e74f01c4060
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-01-16 14:15:52 +0100

    Changement du market

--- assets/market.php
+++ assets/market.php
@@ -1,79 +1,33 @@
 <?php
-switch($_GET['category']) {
-    case 'compositeurs':
-        include("header.php"); ?>
-        <main class="container">
-            <div class="page-header">
-                <h1 class="text-center">Compositeurs</h1>
-            </div>
-        </main>
-        <?php break;
-    case 'interpretes':
-        include("header.php"); ?>
-        <main class="container">
-            <div class="page-header">
-                <h1>Interprètes</h1>
-            </div>
-        </main>
-        <?php break;
-    case 'epoque':
-        include("header.php");?>
-        <main class="container">
-            <div class="page-header">
-                <h1>Epoque</h1>
-            </div>
-        </main>
-        <?php break;
-    case 'chefs_orchestre':
-        include("header.php");?>
-        <main class="container">
-            <div class="page-header">
-                <h1>Chefs d'orchestre</h1>
-            </div>
-        </main>
-        <?php break;
-    case 'orchestres':
-        include("header.php");?>
-        <main class="container">
-            <div class="page-header">
-                <h1>Orchestres</h1>
-            </div>
-        </main>
-        <?php break;
-    case 'instruments':
-        include("header.php");?>
-        <main class="container">
-            <div class="page-header">
-                <h1>Instruments</h1>
-            </div>
-        </main>
-        <?php break;
-    case 'genre':
-        include("header.php");?>
-        <main class="container">
-            <div class="page-header">
-                <h1>Genre</h1>
-            </div>
-        </main>
-        <?php break;
-    default:
-        header('Location: error.php');
+$initial = "";
+if(isset($_GET['initiale'])) {
+    $initial = $_GET['initiale'];
 }
 
+$queries = [
+    "compositeurs" => "SELECT DISTINCT(Nom_Musicien) 
+                      FROM Musicien INNER JOIN Composer ON Musicien.Code_Musicien = Composer.Code_Musicien 
+                      WHERE Nom_Musicien LIKE N'".$initial."';" ,
+    "interpretes" => "SELECT Nom_Musicien FROM Musicien WHERE Nom_Musicien LIKE N'".$initial."';" ,
+    "epoque" => "SELECT Nom_Musicien FROM Musicien WHERE Nom_Musicien LIKE N'".$initial."';" ,
+    "chef_orchestre" => "SELECT Nom_Musicien FROM Musicien WHERE Nom_Musicien LIKE N'".$initial."';" ,
+    "orchestres" => "SELECT Nom_Musicien FROM Musicien WHERE Nom_Musicien LIKE N'".$initial."';" ,
+    "instrument" => "SELECT Nom_Musicien FROM Musicien WHERE Nom_Musicien LIKE N'".$initial."';" ,
+    "compositeurs" => "SELECT Nom_Musicien FROM Musicien WHERE Nom_Musicien LIKE N'".$initial."';" ,
+    "genre" => "SELECT Nom_Musicien FROM Musicien WHERE Nom_Musicien LIKE N'".$initial."';" ,
+];
 
-?>
 
-    <!-- <h4>LA BOUTIQUE DES BG</h4>
-        <form method="post" action=<?php/*$_SERVER['PHP_SELF']*/?>>
-            <select name="alphabet">
-                <option value="" disabled="true" selected="true">Choisir une lettre</option>
-                <?php/*
-                for($alpha = 65; $alpha != 91; $alpha++) {
-                    echo '<option value="' . chr($alpha) . '">' . chr($alpha) . '</option>';
-                }
-                */?>
-            </select>
-            <button class="btn btn-primary btn-lg" type="submit" name="action">Envoyer</button>
-        </form> -->
 
+if(isset($queries[$_GET['category']])) {
+    //TO DO,
+} else {
+    header('Location: error.php');
+}
+include("header.php"); ?>
+    <main class="container">
+        <div class="page-header">
+
+        </div>
+    </main>
 <?php include("footer.php"); ?>
